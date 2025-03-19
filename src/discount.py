20db56edd76488ae199f8af6cfdf8588401bf37d
alex2021-tech <alx00210@gmail.com>
2025-03-19 15:15:34 -0400

    added __init__ file

---
+++ src/discount.py
@@ -0,0 +1,11 @@
+"""
+Module Name: Discount
+Description: Implements bond pricing models using discounting, yield calculations, and duration analysis.
+Date: [2025-03-19]
+"""
+
+# Imports
+import numpy as np
+import pandas as pd
+
+
